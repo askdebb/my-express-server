81f4704cd7b3855277b025370d4c93525e47f44b
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-04-01 12:12:07 +0000

    added bmi cal

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     // console.log(req);
@@ -17,7 +19,22 @@ app.get("/about", function(req, res){
 
 app.get("/services", function(req, res){
     res.send("We create digital solutions to potential clients across the globe");
-})
+});
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    
+   const weight = parseFloat(req.body.weight);
+   let height = parseFloat(req.body.height);
+    height*=height;
+    const result = weight / height;
+    res.send(`The result is ${result} kg/m squared.`);
+
+});
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
